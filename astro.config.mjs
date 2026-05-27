aefa95668ced969d7a2e1bed2b96a732cac288ca
Lucas Souza da Hora <lucassh26@gmail.com>
2026-05-27 05:51:41 -0300

    Testando

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,5 @@ export default defineConfig({
   // Agora sim, com a barra correta na importação acima!
   site: 'https://lucas-da-hora.github.io',
   base: '/portfolio',
+  trailingSlash: 'always'
 });
